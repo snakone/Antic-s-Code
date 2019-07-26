6c31312db4cd52972a5b727cfcbcd9c8d98752ca
Sergio Martínez <snak_one@hotmail.com>
2019-07-26 22:29:13 +0200

    HomeHeader (Missing Responsive)

--- src/app/shared/shared.data.ts
+++ src/app/shared/shared.data.ts
@@ -1,6 +1,32 @@
+import { Category } from './interfaces/interfaces';
+
 export const MENU = [
   { name: 'Home' },
   { name: 'Code' },
   { name: 'Articles' },
   { name: 'Tutorials' }
 ];
+
+export const CATEGORIES: Category[] = [
+  {
+    name: 'Restaurants',
+    icon: 'restaurant_menu',
+    class: 'icon-pink'
+  },
+  {
+    name: 'Places',
+    icon: 'place',
+    class: 'icon-green'
+  },
+  {
+    name: 'Shooping',
+    icon: 'shopping_cart',
+    class: 'icon-orange'
+  },
+  {
+    name: 'Hotels',
+    icon: 'hotel',
+    class: 'icon-red'
+  },
+];
+
